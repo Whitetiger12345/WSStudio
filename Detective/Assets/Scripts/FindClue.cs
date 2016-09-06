4a71a679b60888c8d1061fbab6524a6cbcfa4ea6
karlovvemail <karlovvemail@yandex.ua>
2016-09-06 18:51:33 +0300

    Єто не для комита

---
+++ Detective/Assets/Scripts/FindClue.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FindClue : MonoBehaviour {
+	public GameObject Target;
+	// Use this for initialization
+	void Start () {
+		Target = null;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RaycastHit hit;
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		if (Physics.Raycast (ray, out hit,50.0f)) {
+			Target = hit.transform.gameObject;
+		}
+		if (Target.tag == "Clue" && Input.GetKeyDown(KeyCode.Q)) {
+			Debug.Log ("Clue" + Target.name);
+		}
+	}
+}

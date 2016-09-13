c1eff87189e7c2f9c54ce0fa73eec9f688e9e670
unknown <igorshwarts@yandex.ru>
2016-09-13 19:40:10 +0300

    пиздарики

---
+++ Detective/Assets/Scripts/CoverWall.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoverWall : MonoBehaviour {
+	public Animator anim;
+	public bool cover;
+	void Start () {
+		anim=gameObject.GetComponent<Animator> ();
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+		if(cover)
+		anim.SetFloat ("Turn", Input.GetAxis ("Horizontal"));
+		if (Input.GetKeyDown (KeyCode.X)) {
+			anim.SetBool ("Cover", true);
+			cover = !cover;
+		}
+		if (!cover)
+			anim.SetBool ("Cover", false);
+	}
+}
---
+++ Detective/Assets/Scripts/PushBox.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace UnityStandardAssets.Characters.ThirdPerson
+{
+
+public class PushBox : MonoBehaviour {
+		public Animator anim;
+		public GameObject Target;
+		private Rigidbody rigib;
+		// Use this for initialization
+		void Start () {
+			anim=gameObject.GetComponent<Animator> ();
+		}
+
+		// Update is called once per frame
+		void Update () {
+		rigib=Target.GetComponent<Rigidbody> ();	
+		anim.SetFloat ("Pushing", Input.GetAxis ("Pushing"));
+		if (Input.GetKeyDown (KeyCode.E)) {
+			gameObject.GetComponent<ThirdPersonUserControl> ().enabled = false;
+			gameObject.GetComponent<BoxCollider> ().enabled = true;
+			rigib.mass = 1;
+		}
+			if (Input.GetKeyUp (KeyCode.E)) {
+				gameObject.GetComponent<BoxCollider> ().enabled = false;
+				gameObject.GetComponent<ThirdPersonUserControl> ().enabled = true;
+				rigib.mass = 20000;
+			}
+		}
+
+	}
+}
